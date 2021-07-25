3d68c0f0c933372cbac97aef936ea2fcc9f67954
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-07-25 21:15:49 +0200

    Added slideshow functionality

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -4,9 +4,23 @@ import quoteService from "../../services/QuoteService";
 import Loader from "../../lib/Loader";
 import QuoteCard from "../QuoteCard/QuoteCard";
 import RefreshButton from "../RefreshButton/RefreshButton";
+import TimerOffIcon from '@material-ui/icons/TimerOff';
+import TimerIcon from '@material-ui/icons/Timer';
 
 function HomePage(props) {
     const [quote, setQuote] = useState();
+    const [slideshow, setSlideShow] = useState(false);
+
+    useEffect(() => {
+        let timer;
+        if (slideshow) {
+            timer = setTimeout(() => {
+                fetchRandomQuote()
+            }, 8000);
+        }
+
+        return function cleanup () { clearTimeout(timer); }
+    }, [slideshow])
 
     useEffect(() => {
         fetchRandomQuote()
@@ -16,7 +30,6 @@ function HomePage(props) {
         Loader.showLoader();
         quoteService.getRandomQuote().then(quote => {
             setQuote(quote)
-            // console.log(quote);
             Loader.hideLoader();
         })
     }
@@ -24,7 +37,12 @@ function HomePage(props) {
     return (
         <div className="home">
             <div>
-                <h1 className="title tooltip" data-tip="Display a random quote">Random Quote</h1>
+                <div className="top">
+                    <h1 className="title tooltip" data-tip="Display a random quote">Random Quote</h1>
+                    {slideshow ?
+                        <span data-tip="Stop slideshow" className="tooltip"><TimerOffIcon onClick={() => setSlideShow(false)} className="svgButton"/></span> :
+                        <span data-tip="Start slideshow" className="tooltip"><TimerIcon onClick={() => setSlideShow(true)} className="svgButton"/></span>}
+                </div>
                 {quote && <QuoteCard match={props.match} user={props.user} quote={quote}/>}
             </div>
             <RefreshButton onRefresh={fetchRandomQuote}/>
