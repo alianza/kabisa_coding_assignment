fb52ec51b3af16ab39cd1d6a727600f99d95c373
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-07-25 18:19:01 +0200

    Fix firebase and manifest

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import "./SignIn.scss"
-import firebase from 'firebase';
+import firebase from 'firebase/app';
 import { useHistory } from "react-router-dom";
 
 // Configure FirebaseUI.
